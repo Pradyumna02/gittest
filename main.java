8a7d82e37dec46a7d61e56ef4418048a93a2773b
Pradyumna02 <44702679+Pradyumna02@users.noreply.github.com>
2018-11-03 13:23:05 +0530

    leap year

--- main.java
+++ main.java
@@ -1,35 +1,15 @@
-package power_two;
+package leap_year;
 
 import java.util.Scanner;
 
 public class main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter Nth term for counting power:\n");
-		int value=sc.nextInt();
-		int p;
-		if(value >= 31) {
-			System.out.println("Value must be less than 31");
-		}else {
-			for(int i=1;i<=value;i++) {
-				p=power(i);
-				System.out.println("Power of"+i+" ="+p);
-			}
-		}
-		
+		System.out.println("Enter Year in 4 digit:\n");
+		int year=sc.nextInt();
+		if(year%4==0) 
+			System.out.println(year+" is a leap year");
+		else
+			System.out.println(year+" is not a leap year");
 	}
-
-	private static int power(int n) {
-		// TODO Auto-generated method stub
-		int count=1,base=2;
-		
-			while(n!=0) {
-				count=count*base;
-				--n;
-			}
-			return count;
-		
-		
-	}
-	
 }
